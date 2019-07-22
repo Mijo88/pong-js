c724b339961d754349b7141a8f30c8e374dff9c5
Mitch Jore <mitch_j88@hotmail.com>
2019-07-23 00:08:04 +0200

    Adding flask server for remote viewing

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,6 @@
 import { GameController } from "./modules/GameController.js";
 import { config } from "./config.js";
 
-console.log("CONFIG:", config);
-
-
 function main() {
     const gc = new GameController(config);
 }
